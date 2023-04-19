2d1abb166ce303df4b8b0ab3b5846fe6fcd8d8b4
Mohammed Abubakar <mohammedfahim363@yahoo.com>
2023-04-18 21:09:33 -0700

    completelink

---
+++ javascript/code-challenges/reverse-ll/linked-list/index.js.js
@@ -0,0 +1,40 @@
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insert(value) {
+    const node = new Node(value);
+    node.next = this.head;
+    this.head = node;
+  }
+
+  includes(value) {
+    let current = this.head;
+    while (current) {
+      if (current.value === value) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  }
+
+  toString() {
+    let result = '';
+    let current = this.head;
+    while (current) {
+      result += `{ ${current.value} } -> `;
+      current = current.next;
+    }
+    result += 'NULL';
+    return result;
+  }
+}
